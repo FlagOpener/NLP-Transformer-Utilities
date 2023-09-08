85f65af34bcaef39f62dae0f9efbf5b3a5ded59c
FlagOpener <127030018+FlagOpener@users.noreply.github.com>
2023-09-08 21:42:48 +0000

    xen/hvc: Collapse error logic.

--- qa_extract_twitter.py
+++ qa_extract_twitter.py
@@ -2,4 +2,12 @@
 # -*- coding: utf-8 -*-
 
 #
-# Cop
+# Copyright 2019 Guenter Bartsch
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
+# U
