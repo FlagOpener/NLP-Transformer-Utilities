6d972f0869a8502424b44941b3d35f844a491234
FlagOpener <127030018+FlagOpener@users.noreply.github.com>
2023-10-20 22:55:41 +0000

    Video support for iPad

--- update_twitter_corpus.sh
+++ update_twitter_corpus.sh
@@ -1,1 +1,6 @@
-#
+#!/bin/bash
+
+# ./twitterscrape.py -v -s 2019-06-01 twitter201906 /home/bofh/projects/ai/data/corpora/en/twitter/twitter_sources.txt
+
+#   -U USER_STATS, --user-stats=USER_STATS
+#                         user stats file, defa
