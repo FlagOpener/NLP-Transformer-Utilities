91449f6baf483de3bff07392c5f0f88e34909ccc
FlagOpener <127030018+FlagOpener@users.noreply.github.com>
2023-10-22 06:43:43 +0000

    Updated Polish translation.

--- update_twitter_corpus.sh
+++ update_twitter_corpus.sh
@@ -3,4 +3,7 @@
 # ./twitterscrape.py -v -s 2019-06-01 twitter201906 /home/bofh/projects/ai/data/corpora/en/twitter/twitter_sources.txt
 
 #   -U USER_STATS, --user-stats=USER_STATS
-#                         user stats file, defa
+#                         user stats file, default: /home/bofh/projects/ai/data/corpora/en/twitter/user_stats.json
+
+
+./twitterscrape.py -l de -s 2019-08-01 twitter_de_201908 -U /home/bofh/projects/ai/data/corpora/en/twitter/user_stats_de.json /home/bofh/projects/ai/data/corpo
