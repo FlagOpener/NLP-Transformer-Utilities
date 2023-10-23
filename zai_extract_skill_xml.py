1aeafa2e8e9e6755ede0f2848b8c4fc3e6ed7688
FlagOpener <127030018+FlagOpener@users.noreply.github.com>
2023-10-23 12:18:00 +0000

    Update train.py

--- zai_extract_skill_xml.py
+++ zai_extract_skill_xml.py
@@ -13,4 +13,19 @@
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# 
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+#
+
+import os
+import sys
+import codecs
+import time
+import logging
+import datetime
+import imp
+
+from optparse import OptionParser
+
+from nlt
